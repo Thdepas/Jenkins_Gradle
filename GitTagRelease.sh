6c5e824db573ccccbeb17885cf4620b6b3ae1051
thdepas <thdepas@gmail.com>
2022-01-25 16:53:03 +0100

    kowed

--- GitTagRelease.sh
+++ GitTagRelease.sh
@@ -4,37 +4,32 @@ VERSION=`git tag --list --sort=version:refname '1*' | tail -1`
 
 VERSION_BITS=(${VERSION//./})
 
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
+MAJOR=${VERSION_BITS[0]}
+MINOR=${VERSION_BITS[1]}
+PATCH=${VERSION_BITS[2]}
 
 NOTE=""
 
-MAJOR=`git log -1 --pretty=%B | grep -i "major"`
-MINOR=`git log -1 --pretty=%B | grep -i "minor"`
-
-if [ "$MAJOR" ];
+if git log -1 --pretty=%B |  -Fxq "[MAJOR]";
 then
     echo "Update major version"
     NOTE="Update major version"
-    VNUM1=$(($VNUM1+1))
-    VNUM3=0
-elif [  "$MINOR" ];
+    MAJOR=$(($MAJOR+1))
+    MINOR=0
+    PATCH=0
+elif git log -1 --pretty=%B | grep -Fxq "[MINOR]";
 then
     echo "Update minor version"
     NOTE="Update minor version"
-    VNUM2=$(($VNUM2+1))
-    VNUM3=0
+    MINOR=$(($MINOR+1))
+    PATCH=0
 else
     echo "Update patch version"
     NOTE="Update patch version"
-    VNUM3=$(($VNUM3+1))
+    VNUM3=$(($PATCH+1))
 fi
 
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-
-echo $NEW_TAG
-echo $VERSION
+NEW_TAG="$MAJOR.$MINOR.$PATCH"
 
 gh release create "$NEW_TAG" -n "$NOTE"
 gh release upload "$NEW_TAG" /var/jenkins_home/workspace/Caersar/Jenkins_Gradle/build/libs/caesars-cipher.jar
