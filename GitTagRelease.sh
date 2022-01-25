30479a6e5b07e0710087bd5b43a733c1a75777b0
thdepas <thdepas@gmail.com>
2022-01-25 16:26:03 +0100

    minor

--- GitTagRelease.sh
+++ GitTagRelease.sh
@@ -8,28 +8,33 @@ VNUM1=${VERSION_BITS[0]}
 VNUM2=${VERSION_BITS[1]}
 VNUM3=${VERSION_BITS[2]}
 
-MAJOR=`git log --format=%B -n 1 HEAD | grep '#major'`
-MINOR=`git log --format=%B -n 1 HEAD | grep '#minor'`
+NOTE=""
+
+MAJOR=`git log -1 --pretty=%B | grep -i "major"`
+MINOR=`git log -1 --pretty=%B | grep -i "minor"`
 
 if [ "$MAJOR" ];
 then
     echo "Update major version"
+    NOTE="Update major version"
     VNUM1=$(($VNUM1+1))
     VNUM3=0
 elif [  "$MINOR" ];
 then
     echo "Update minor version"
+    NOTE="Update minor version"
     VNUM2=$(($VNUM2+1))
     VNUM3=0
 else
     echo "Update patch version"
+    NOTE="Update patch version"
     VNUM3=$(($VNUM3+1))
 fi
 
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 
-NEW_TAG="${VNUM1}.${VNUM2}.${VNUM3}"
-
+echo $NEW_TAG
 echo $VERSION
 
-gh release create "$VERSION" -n "patch"
-gh release upload "$VERSION" /var/jenkins_home/workspace/Caersar/Jenkins_Gradle/build/libs/caesars-cipher.jar
+gh release create "$NEW_TAG" -n "$NOTE"
+gh release upload "$NEW_TAG" /var/jenkins_home/workspace/Caersar/Jenkins_Gradle/build/libs/caesars-cipher.jar
